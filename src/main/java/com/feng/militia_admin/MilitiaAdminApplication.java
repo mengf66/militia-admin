11fa08adf7492af8f5578da414682de7625904c7
fengguizhong.888__dcar <fengguizhong.888@dcarlife.com>
2026-06-04 17:37:58 +0800

    init:初始化仓库

---
+++ src/main/java/com/feng/militia_admin/MilitiaAdminApplication.java
@@ -0,0 +1,13 @@
+package com.feng.militia_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilitiaAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilitiaAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/feng/militia_admin/MilitiaAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feng.militia_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilitiaAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
